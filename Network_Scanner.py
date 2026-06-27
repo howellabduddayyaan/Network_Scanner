7f97a58d0a9136388cdb625e9d3e6ff23556696a
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-06-27 20:20:17 +0200

    Feat : Range added, trying to add Network scan progress bar. Scan seems to freeze in terminal

--- Network_Scanner.py
+++ Network_Scanner.py
@@ -4,7 +4,7 @@
 
 import subprocess
 import socket
-import sys
+
 
 print("=== Network Scanner ===")
 
@@ -14,3 +14,46 @@ print("\nScanning network...\n")
 
 total = 254
 
+try:
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+    print(f"Your IP: {local_ip}\n")
+except:
+    pass
+
+for i in range(1, 255):
+
+    ip = f"{network}.{i}"
+
+# _________________________________________________________________________________________________
+
+
+print(f"Scanning {ip}...", end="\r")
+
+    try:
+        
+        result = subprocess.run(
+            ["ping", "-n", "1", "-w", "100", ip],
+            capture_output=True,
+            text=True
+        )
+
+        if "TTL=" in result.stdout:
+
+            try:
+                hostname = socket.gethostbyaddr(ip)[0]
+            except:
+                hostname = "Unknown"
+
+            print(f"{ip}  →  {hostname} (ONLINE)")
+
+    except:
+        pass
+
+    print("\n\nScan Complete.")
+
+
+
+
+
+# _________________________________________________________________________________________________
